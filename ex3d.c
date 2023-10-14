314fc90e8e78a895472356059e13e1e2da6a39ad
Cyril Jacob <131938772+CJ445@users.noreply.github.com>
2023-10-14 13:46:14 +0530

    Update ex3d.c

--- ex3d.c
+++ ex3d.c
@@ -1,21 +1,17 @@
-// Type your URK and name here
+// Type your URK and name
 #include<stdio.h>  
 int main()  
 {  
-    int num, row, col, count = 1;  
-    printf("Enter number of rows for Floyd's Triangle : ");  
-    scanf("%d", &num);  
-  
-    printf("\n");  
-    for(row = 1; row <= num; row++)  
-    {  
-        for(col = 1; col <= row; col++)  
-        {  
-            printf("%d  ", count++);  
-        }  
-        printf("\n");  
+    int i, j, n;
+    printf("Enter number : ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            printf("%d",j);
+        }
+        printf("\n");
     }
     printf("\n");
-    printf("Replace your URK and name");
+    printf("Replace with your URK and name");
     return 0;
 }
