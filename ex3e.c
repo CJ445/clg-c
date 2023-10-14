2c515d951351b6d2b3334d7c9464f105ec47339b
Cyril Jacob <131938772+CJ445@users.noreply.github.com>
2023-10-14 13:45:20 +0530

    Update ex3e.c

---
+++ ex3e.c
@@ -0,0 +1,21 @@
+// Type your URK and name here
+#include<stdio.h>  
+int main()  
+{  
+    int num, row, col, count = 1;  
+    printf("Enter number of rows for Floyd's Triangle : ");  
+    scanf("%d", &num);  
+  
+    printf("\n");  
+    for(row = 1; row <= num; row++)  
+    {  
+        for(col = 1; col <= row; col++)  
+        {  
+            printf("%d  ", count++);  
+        }  
+        printf("\n");  
+    }
+    printf("\n");
+    printf("Replace with your URK and name");
+    return 0;
+}
