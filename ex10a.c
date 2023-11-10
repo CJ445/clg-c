5fc904d8a34c23a516e4478abcf2ba22301ee935
Akhil <145592032+AKA-Akhil@users.noreply.github.com>
2023-11-10 22:43:44 +0530

    Create ex10a.c

---
+++ ex10a.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+FILE *sourceFile, *destFile;
+char ch;
+sourceFile = fopen("source.txt", "r"); // Open the source file in read mode
+if (sourceFile == NULL) {
+perror("Error opening source file");
+return 1;
+}
+destFile = fopen("destination.txt", "w"); // Open the destination file in write mode
+if (destFile == NULL) {
+perror("Error opening destination file");
+
+41
+
+fclose(sourceFile); // Close the source file before exiting
+return 1;
+}
+while ((ch = fgetc(sourceFile)) != EOF) {
+fputc(ch, destFile); // Write each character from source to destination
+}
+printf("File copied successfully!\n");
+fclose(sourceFile);
+fclose(destFile);
+return 0;
+}
